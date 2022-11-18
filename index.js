ec19ba50188fe7af6c2d2d30af41773f3e0b8003
meng <mengzhou44@gmail.com>
2022-11-17 20:01:20 -0700

    commit

---
+++ index.js
@@ -0,0 +1,74 @@
+const pdf = require("pdf-creator-node");
+const html = `
+                <!DOCTYPE html>
+                <html>
+                    <head>
+                        <meta charset="utf-8" />
+                        <title>Hello world!</title>
+                         <style>
+                           .list {
+                              color: red; 
+                              border: 1px solid green;
+                           }
+
+                           .container {
+                              display: flex; 
+                              justify-content: flex-start; 
+                           }
+
+                           .item {
+                              margin: 20px; 
+                              border: 1px solid blue;
+                           }
+                         </style>
+                    </head>
+                    <body>
+                        <h1 class='list'>User List</h1>
+                        <div class='container'>
+                            <div class='item'>
+                                <p>Name: David </p>
+                                <p>Age: 26 </p>
+                            </div>
+
+                            <div class='item'>
+                                <p>Name: Mark </p>
+                                <p>Age: 31 </p>
+                            </div>
+                        </div>
+                    </body>
+                </html>
+            `
+const options = {
+    format: "A3",
+    orientation: "portrait",
+    border: "10mm",
+    header: {
+        height: "45mm",
+        contents: '<div style="text-align: center;">Author: Shyam Hajare</div>'
+    },
+    footer: {
+        height: "28mm",
+        contents: {
+            first: 'Cover page',
+            2: 'Second page', // Any page number is working. 1-based index
+            default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
+            last: 'Last Page'
+        }
+    }
+};
+
+var document = {
+    html,
+    data: {},
+    path: "./output.pdf",
+    type: "",
+  };
+
+  pdf
+  .create(document, options)
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
