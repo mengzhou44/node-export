dc025ca60cef28bc2c48e12bba065a49b61fcf4c
meng <mengzhou44@gmail.com>
2022-11-23 13:20:20 -0700

    commit

--- src/create-pdf.ts
+++ src/create-pdf.ts
@@ -1,24 +1,30 @@
-import  puppeteer from 'puppeteer';
+import  pdf, {CreateOptions} from 'html-pdf'
 
 export async function createPDF(htmlContent: string): Promise<Buffer> {
    try {
 
-        const browser = await puppeteer.launch();        
-        
-        const page = await browser.newPage();
-        await page.setContent(htmlContent,  {waitUntil: 'domcontentloaded'})
-        await page.emulateMediaType('screen');
-        
-        const pdf = await page.pdf({
-            path: 'result.pdf',
-            margin: { top: '100px', right: '50px', bottom: '100px', left: '50px' },
-            printBackground: true,
-            format: 'A4',
-        });
-        
-        await browser.close()
-
-        return pdf
+     const options: CreateOptions = {
+          format: "A3",
+          orientation: "portrait",
+          border: "10mm",
+          header: {
+              height: "45mm"
+          },
+          footer: {
+              height: "28mm"
+          }
+      };
+      
+      return new Promise((resolve, reject)=> {
+          pdf.create(htmlContent, options).toBuffer((err,buffer)=> {
+                  if (err) {
+                     console.log(err)
+                     reject(err)
+                  }
+                  resolve(buffer)
+          })
+      })
+       
    } catch(err) {
         console.log('Error occured when creating PDF file')
         throw err
